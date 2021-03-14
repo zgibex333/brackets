043c8ba62d6e551e1938bb8b0b261b182de449f4
Denis Czaplia <zgibex333@gmail.com>
2021-03-14 17:02:25 +0300

    feat: solution was added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+        let depth = [];
+    for (let bracket of str) {
+            let bracketsIndex = brackets.indexOf(bracket)
+
+            if (bracketsIndex % 2 === 0) {
+                
+                if (bracket === brackets[bracketsIndex + 1] && depth[depth.length - 1] === bracketsIndex){
+                    depth.pop();
+                } else if (bracket === brackets[bracketsIndex + 1] && depth[depth.length - 1] !== bracketsIndex) {
+                    depth.push(bracketsIndex)
+                }
+                else{
+                    depth.push(bracketsIndex)
+                }
+            } 
+            else {
+                if (depth.pop() !== bracketsIndex-1){
+                    return false;
+                }
+            }
+            
+        }
+        return depth.length === 0
+
 }
